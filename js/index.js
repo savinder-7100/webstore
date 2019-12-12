0948e8e0ca6c6327276e877452a67c89d85b02e8
Savinder Kaur <n01380755@nj-00118-024.humber.org>
2019-12-12 12:36:23 -0500

    Last commit

---
+++ js/index.js
@@ -0,0 +1,119 @@
+const shoppingCart = [
+    {itemsid:101, qty:1}
+];
+
+// Array items is defined here. 
+const items = [
+    { //0
+        id: 101,
+        name: `Aloe Camisole`,
+        image: `https://cdn.shopify.com/s/files/1/1132/3440/products/9DC9EFE4-E3C8-48CD-B817-EAFB67983FE8_large.jpg?v=1575796898`,
+        description: `The Aloe Camisole in linen rayon features a gathered bust panel, adjustable straps, and has a dressy fit.`,
+        price: `79.00`,
+        quantity: 25,
+        category: `Fashion`,
+        size: `M`,
+        brand: `ZULU & ZEPHYR`,
+        review: 5
+    }, { //1
+        id: 102,
+        name: `Alva Msch Est Tee // Black/White`,
+        image: `https://cdn.shopify.com/s/files/1/1132/3440/products/33B0A1CC-399A-47AF-B208-0C24CE4ADBFA_large.jpeg?v=1574475055`,
+        description: `Boyfriend Fitted T-shirt with a roll-up sleeve with the MSCH Logo. `,
+        price: `199.00`,
+        quantity: 0,
+        category: `Fashion`,
+        size: `L`,
+        brand: `MSCH COPENHAGEN`,
+        review: 2
+    }, { //2
+        id: 103,
+        name: `Atomic Jumper // Lime Green`,
+        image: `https://cdn.shopify.com/s/files/1/1132/3440/products/5C85ED49-B99F-430D-B60A-7CE542D00BA8_large.jpeg?v=1574474583`,
+        description: `Slim fit deep rib knitted crew neck jumper. Long slim sleeves. Length hits below the hip. Available in Bright Violet and Lime Green. `,
+        price: `162.00`,
+        quantity: 30,
+        category: `Fashion`,
+        size: `S`,
+        brand: `RICOCHET`,
+        review: 3.5
+    }, { //3
+        id: 104,
+        name: `Bias Skirt`,
+        image: `https://cdn.shopify.com/s/files/1/1132/3440/products/image_abe45ed8-5d17-42ff-ac9f-83d7b1651933_large.jpg?v=1571709560`,
+        description: `Undoubtedly the must-have staple in any wardrobe this season, the Bias Skirt now comes in a bold Snake Print. A mid-length cut on the bias to flatter, this skirt is easy to wear dressed up or down for every occasion. Made to be an effortless style with an elasticated waistband you won’t want to take it off. `,
+        price: `159.00`,
+        quantity: 10,
+        category: `Fashion`,
+        size: `XS`,
+        brand: `REMAIN`,
+        review: 4.2
+    }, { //4
+        id: 105,
+        name: `Blossom Dress`,
+        image: `https://cdn.shopify.com/s/files/1/1132/3440/products/9606C6DB-2234-4EA9-A65B-3603A5733F93_large.jpg?v=1575796865`,
+        description: `The Blossom Dress in cotton voile features an exclusive print. With a cross front skirt and neckline, and a wide smocked waistband, this has a relaxed casual fit.`,
+        price: `154.00`,
+        quantity: 50,
+        category: `Fashion`,
+        size: `L`,
+        brand: `ZULU & ZEPHYR`,
+        review: 4.9
+    }, { //5
+        id: 106,
+        name: `Dublin Cropped Pant`,
+        image: `https://cdn.shopify.com/s/files/1/1132/3440/products/554D8741-F7DB-423C-8813-A2F9522B1C43_large.jpeg?v=1571709566`,
+        description: `Bright Crystal is a fresh, sensual blend of refreshing chilled yuzu and pomegranate mingled with soothing blossoms of peony, magnolia, and lotus flower, warmed with notes of musk and amber.`,
+        price: `126.00`,
+        quantity: 40,
+        category: `Fashion`,
+        size: `S`,
+        brand: `BETTY BASICS`,
+        review: 3.2
+    }, { //6
+        id: 107,
+        name: `Duct Tape Tee Dress`,
+        image: `https://cdn.shopify.com/s/files/1/1132/3440/products/5D8B0722-38B4-4BC0-8B71-CA1932509022_large.jpg?v=1574292441`,
+        description: `Basic Tee Dress`,
+        price: `98.00`,
+        quantity: 20,
+        category: `Fashion`,
+        size: `M`,
+        brand: `STOLEN GIRLFRIENDS CLUB`,
+        review: 3.8
+    }, { //8
+        id: 109,
+        name: `Freckles Maxi Scarf`,
+        image: `https://cdn.shopify.com/s/files/1/1132/3440/products/84788A05-6BEB-4C77-A680-0ABEDF90D621_large.jpg?v=1576144981`,
+        description: `How cute is the freckle print? The much loved maxi works with all your basics - super handy as a wrap at the beach with your bikini or for a little bit of warmth day or night.`,
+        price: `38.00`,
+        quantity: 33,
+        category: `Fashion`,
+        size: `S`,
+        brand: `SOPHIE`,
+        review: 2.2
+    }, { //9
+        id: 110,
+        name: `Gather Dress`,
+        image: `https://cdn.shopify.com/s/files/1/1132/3440/products/image_2a8ed2e4-e7bf-4226-8fa2-0ac0cfcd5888_large.jpg?v=1575353166`,
+        description: `An oversized dress with gathered waist and a cropped sleeve, made from lightweight organic cotton jersey.`,
+        price: `120.00`,
+        quantity: 30,
+        category: `Fashion`,
+        size:  `XS`,
+        brand: `KOWTOW`,
+        review: 1
+    }, { //10
+        id: 111,
+        name: `Goldie Skirt`,
+        image: `https://cdn.shopify.com/s/files/1/1132/3440/products/image_7944c896-23a8-473a-b97a-fc67cee8a509_large.jpg?v=1571709560`,
+        description: `The Goldie Skirt in linen rayon features an exclusive print. With a clean waistline, panelled body, and front split, this has a clean, dressy fit.`,
+        price: `65.00`,
+        quantity: 100,
+        category: `Fashion`,
+        size: `XS`,
+        brand: `ZULU & ZEPHYR`,
+        review: 4.4
+    }
+];
+
